8735a4aba4de9c823ac3ca2a3894804dffb17cb3
Aaron Piotrowski <aaron@trowski.com>
2022-04-08 17:07:27 -0500

    Update example

--- examples/server.php
+++ examples/server.php
@@ -2,56 +2,66 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Amp\Http\Server\RequestHandler\CallableRequestHandler;
+use Amp\ByteStream;
+use Amp\Http\Server\DefaultErrorHandler;
+use Amp\Http\Server\Request;
+use Amp\Http\Server\RequestHandler\ClosureRequestHandler;
 use Amp\Http\Server\Response;
 use Amp\Http\Server\Router;
-use Amp\Http\Server\Server;
+use Amp\Http\Server\SocketHttpServer;
 use Amp\Http\Server\StaticContent\DocumentRoot;
 use Amp\Http\Status;
+use Amp\Log\ConsoleFormatter;
+use Amp\Log\StreamHandler;
 use Amp\Socket;
-use Psr\Log\NullLogger;
+use Monolog\Logger;
+use Monolog\Processor\PsrLogMessageProcessor;
+use function Amp\trapSignal;
 
 // Run this script, then visit http://localhost:1337/ in your browser.
 
-Amp\Loop::run(function () {
-    $sockets = [
-        Socket\listen("0.0.0.0:1337"),
-        Socket\listen("[::]:1337"),
-    ];
+$logHandler = new StreamHandler(ByteStream\getStdout());
+$logHandler->pushProcessor(new PsrLogMessageProcessor());
+$logHandler->setFormatter(new ConsoleFormatter());
+$logger = new Logger('server');
+$logger->pushHandler($logHandler);
 
-    $documentRoot = new DocumentRoot(__DIR__ . '/public');
+$server = new SocketHttpServer($logger);
+$server->expose(new Socket\InternetAddress("0.0.0.0", 1337));
+$server->expose(new Socket\InternetAddress("[::]", 1337));
 
-    $router = new Router;
-    $router->setFallback($documentRoot);
-    $router->addRoute('GET', '/', new CallableRequestHandler(function () {
-        // This can also be in a index.htm file, but we want a demo that uses the router.
-        $html = <<<HTML
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Example</title>
-        <link rel="stylesheet" href="/style.css"/>
-    </head>
-    
-    <body>
-        <div>
-            Hello, World!
-        </div>
-    </body>
-</html>
-HTML;
+$errorHandler = new DefaultErrorHandler();
 
-        return new Response(Status::OK, ['content-type' => 'text/html; charset=utf-8'], $html);
-    }));
+$documentRoot = new DocumentRoot($server, $errorHandler, __DIR__ . '/public');
 
-    $server = new Server($sockets, $router, new NullLogger);
+$router = new Router($server, $errorHandler);
+$router->setFallback($documentRoot);
+$router->addRoute('GET', '/', new ClosureRequestHandler(function (Request $request): Response {
+    // This can also be in a index.htm file, but we want a demo that uses the router.
+    $html = <<<HTML
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Example</title>
+            <link rel="stylesheet" href="/style.css"/>
+        </head>
+        
+        <body>
+            <div>
+                Hello, World!
+            </div>
+        </body>
+        </html>
+        HTML;
 
-    yield $server->start();
+    return new Response(Status::OK, ['content-type' => 'text/html; charset=utf-8'], $html);
+}));
 
-    // Stop the server gracefully when SIGINT is received.
-    // This is technically optional, but it is best to call Server::stop().
-    Amp\Loop::onSignal(SIGINT, function (string $watcherId) use ($server) {
-        Amp\Loop::cancel($watcherId);
-        yield $server->stop();
-    });
-});
+$server->start($router, $errorHandler);
+
+// Await SIGINT or SIGTERM to be received.
+$signal = trapSignal([\SIGINT, \SIGTERM]);
+
+$logger->info(sprintf("Received signal %d, stopping HTTP server", $signal));
+
+$server->stop();
